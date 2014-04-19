e6ed80cf988a712d8f3b02cd7861d17480c1c409
Jeff <jeffburg@jeffburg.com>
2014-04-19 15:47:08 -0700

    Added All the Views and Mockups
    Added All the Views and Mockups

---
+++ SaturdayApp/SaturdayApp/SaturdayApp/EBCreateBroadcastViewController.m
@@ -0,0 +1,38 @@
+//
+//  EBCreateBroadcastViewController.m
+//  SaturdayApp
+//
+//  Created by Jeffrey Bergier on 4/19/14.
+//  Copyright (c) 2014 BharatJeffSimer. All rights reserved.
+//
+
+#import "EBCreateBroadcastViewController.h"
+
+@interface EBCreateBroadcastViewController ()
+
+@end
+
+@implementation EBCreateBroadcastViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SaturdayApp/SaturdayApp/SaturdayApp/EBSelfieViewController.h
@@ -0,0 +1,13 @@
+//
+//  EBSelfieViewController.h
+//  SaturdayApp
+//
+//  Created by Jeffrey Bergier on 4/19/14.
+//  Copyright (c) 2014 BharatJeffSimer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EBSelfieViewController : UIViewController
+
+@end
