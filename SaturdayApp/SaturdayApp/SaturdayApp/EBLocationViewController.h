d05a00f0b732f095b42e2702f0a8e590d539c789
simer <simer.sahani@gmail.com>
2014-04-19 15:41:15 -0700

    Initial View Controller

---
+++ SaturdayApp/SaturdayApp/SaturdayApp/EBLocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  EBLocationViewController.h
+//  SaturdayApp
+//
+//  Created by michael on 4/19/14.
+//  Copyright (c) 2014 BharatJeffSimer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EBLocationViewController : UIViewController
+
+@end
